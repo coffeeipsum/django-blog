16dbf68fb449b33d95c62f367fa36816183f4fcb
Anne Marie Johnston <annemarie.johnston@outlook.com>
2018-08-17 10:11:23 +0000

    updated settings.py to reflect new installed apps and allowed hosts

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import Post
 
+
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'conent', 'image', 'tag', 'published_date')
+        fields = ('title', 'content', 'image', 'tag', 'published_date')
--- posts/views.py
+++ posts/views.py
@@ -1,44 +1,37 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
 
-# Create your views here.
+
 def get_posts(request):
     """
-    this creates a View which returns a List of Posts that were 
-    published prior to 'now' and will render them
-    to the "blogposts.html" template 
+    Create a view that will return a
+    list of Posts that were published prior to'now'
+    and render them to the 'blogposts.html' template
     """
-    
-    # __lte= means 'less than or equal to'
-    posts = Post.Object.filter(published_date__lte=timezone.now()).order_by('-published_date')
-        return render(request, "blogposts.html", {'posts': posts})
-    
-    
-    
-    
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+                                ).order_by('-published_date')
+    return render(request, "blogposts.html", {'posts': posts})
+
+
 def post_detail(request, pk):
     """
-    this creates a View which returns a single Post object based on the post ID (pk)
-    and will render it to the "postdetail.html" template.
-    Or return a 404 error, if post not found.
+    Create a view that return a single
+    Post object based on the post ID and
+    and render it to the 'postdetail.html'
+    template. Or return a 404 error if the
+    post is not found
     """
-    
     post = get_object_or_404(Post, pk=pk)
-    post.views +=1
+    post.views += 1
     post.save()
     return render(request, "postdetail.html", {'post': post})
 
 
 def create_or_edit_post(request, pk=None):
-    """
-    this creates a View which allows us to edit or create a post depending
-    if the post ID is null or not
-    """
-    
     post = get_object_or_404(Post, pk=pk) if pk else None
-    if request.method== "POST":
+    if request.method == "POST":
         form = BlogPostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save()
